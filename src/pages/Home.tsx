640b202ef771a8037befd5d2e62d1ded23c8f8ec
Adelabu Segun Joshua <159425049+Shegzboyer12@users.noreply.github.com>
2025-09-15 11:57:00 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,11 @@ import ProductCard from "../components/ProductCard";
 const Home = () => {
   return (
     <div>
-      <section className="bg-pink-100 py-20 text-center">
-        <h1 className="text-5xl font-bold text-pink-800">Welcome to SweetTreats Bakery</h1>
-        <p className="mt-4 text-lg text-gray-700">Delicious pastries, cakes, and bread made with love.</p>
+      <section className="bg-amber-400 py-20 text-center">
+        <h1 className="text-5xl font-bold text-amber-950">Welcome to SweetTreats Bakery</h1>
+        <p className="mt-4 text-lg text-gray-700">Deliciousness made with love.</p>
         <Link to="/products">
-          <button className="mt-6 px-6 py-3 bg-pink-600 text-white rounded-lg hover:bg-pink-700">
+          <button className="mt-6 px-6 py-3 bg-amber-700 text-white rounded-lg hover:bg-amber-800">
             View All Products
           </button>
         </Link>
@@ -23,7 +23,7 @@ const Home = () => {
           ))}
         </div>
         <div className="text-center mt-8">
-          <Link to="/products" className="text-pink-600 hover:underline">See More →</Link>
+          <Link to="/products" className="text-amber-600 hover:underline">See More →</Link>
         </div>
       </section>
     </div>
