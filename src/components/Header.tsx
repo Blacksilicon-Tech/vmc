bec183185ea8b3a3558352f30c82386f11295ec7
Adelabu Segun Joshua <159425049+Shegzboyer12@users.noreply.github.com>
2025-09-15 11:55:19 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-pink-200 text-gray-800 shadow-md">
+    <header className="bg-amber-900 text-gray-800 shadow-md">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <h1 className="text-2xl font-bold">🍰 Vmc Squared Limited</h1>
+        <Link to="/"><h1 className="text-2xl font-bold">🍰 Vicfield</h1></Link>
         <nav className="space-x-6">
-          <Link to="/" className="hover:text-pink-600 text-lg font-semibold">Home</Link>
-          <Link to="/products" className="hover:text-pink-600 text-lg font-semibold">Products</Link>
-          <Link to="/contact" className="hover:text-pink-600 text-lg font-semibold">Contact</Link>
+          <Link to="/" className="hover:text-amber-400 text-lg font-semibold">Home</Link>
+          <Link to="/products" className="hover:text-amber-400 text-lg font-semibold">Products</Link>
+          <Link to="/contact" className="hover:text-amber-400 text-lg font-semibold">Contact</Link>
         </nav>
       </div>
     </header>
