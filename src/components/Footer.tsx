77c3f47f71aa65f19ad9ed7eb722e35653d939e5
Adelabu Segun Joshua <159425049+Shegzboyer12@users.noreply.github.com>
2025-09-15 11:55:37 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-pink-200 text-center py-4 mt-10">
+    <footer className="bg-amber-400 text-center py-4 mt-10">
       <p className="text-gray-700">© {new Date().getFullYear()} VMC SQUARED LIMITED. All rights reserved.</p>
     </footer>
   );
